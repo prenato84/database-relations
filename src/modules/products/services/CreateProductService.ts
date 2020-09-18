65043c9adb14afc17c01024b5f442aba71bf0a16
Paulo Castro <prenato84@gmail.com>
2020-09-17 23:24:03 -0300

    All tests passed

--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,28 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository,
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
     // TODO
+    const productExists = await this.productsRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError(
+        'The product name provided already exists in the database',
+      );
+    }
+
+    const product = await this.productsRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    return product;
   }
 }
 
